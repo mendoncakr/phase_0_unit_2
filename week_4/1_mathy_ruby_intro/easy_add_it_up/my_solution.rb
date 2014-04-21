9a3e9575b9e6d4af3392a2f914b61502644b43e1
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-04-21 00:16:43 -0400

    completed easy add it up

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -6,20 +6,39 @@
 
 # I worked on this challenge [by myself, with: ].
 
-# 1. Pseudocode
+# 1. Pseudocode for total
+# define total method that adds items of a list
+# input should be an array
+# output should be number
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
+# Pseudocode for sentence_maker
+# define sentence_maker method whose input is an array
+# iterate through array and join each string with a space
+# add period to end of list
+# capitalize first letter
 
 # 2. Initial Solution
 
+def total(array)
+	sum = 0
+	array.each {|num| sum += num}
+	return sum
+end
 
-
+def sentence_maker(array)
+	array << '.' 				#append period to end of list
+	sentence = array.join(' ')  #
+	sentence.capitalize
+end
 
 # 3. Refactored Solution
-
-
+def sentence_maker(array)
+	sentence = array.join(' ')
+	return (sentence.capitalize + '.')
+end
 
 # 4. Reflection 
+# Since it's been over 3 weeks and I haven't had much ruby practice this little exercise was great at getting me back into the groove of ruby.
+# I completed this exercise quite quickly with only minimal research of the internet for array methods, but when I went to test my code, I was getting 4 errors.
+# For some reason, I thought this was an error with RSPEC and spent ~45 minutes trying to determine if it was installed correctly. Turns out it was 
+# installed correctly, but that my code was wrong! I had a puts statement instead of return! That was definitely the most frustrating part.
