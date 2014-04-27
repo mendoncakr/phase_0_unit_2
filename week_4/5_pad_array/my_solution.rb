0431c14532a8576a04f79ce7f86a6362a567d3f7
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-04-27 12:43:03 -0400

    updated pad challenge

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -8,13 +8,46 @@
 
 # 1. Pseudocode
 
-# What is the input?
+# What is the input? 
+	# One is the integer for the # of spaces to be added to the array
+	# Second is an optional argument of any data type that is the item to be added. Default is nil
 # What is the output? (i.e. What should the code return?)
+	# pad will return a copy of the original array, while pad! will return a modified version of the original array
 # What are the steps needed to solve the problem?
+# modify class Array
+# define new method (pad)
+	# 
 
 
 # 2. Initial Solution
+class Array
 
+	def pad!(spaces, filler = nil)
+		arrayCopy = []
+		arrayLength = self.length
+		newSpaces = spaces - arrayLength
+		if spaces <= arrayLength 
+			return self
+		else
+			newSpaces.times { self << filler }
+			return self
+		end
+		puts self
+	end
+
+
+	def pad!(spaces, filler = nil)
+		arrayLength = self.length
+		newSpaces = spaces - arrayLength
+		if spaces <= arrayLength 
+			return self
+		else
+			newSpaces.times { self << filler }
+			return self
+		end
+		puts self
+	end
+end
 
 
 
