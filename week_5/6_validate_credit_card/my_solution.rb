eaceb5d69f9b27ab7109ba9b39d548c9b61d735a
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-04 12:31:37 -0400

    finished pseudocode

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -5,9 +5,20 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: 16 digit integer
+# Output: boolean, representing validity of credit card
 # Steps:
+#initialize class with 16 digit integer
+	# raise error if length is not equal to 16
+#def double_digit method
+	#method should double every other digit of the 16 digits starting from the second to last
+#define digit_sum
+	# method should sum both skipped digits and doubled digits. 
+	# splitting the integers with 2 digits		
+#define valid?
+	#determine if sum from above method is a multiple of 10
+		#if true, return true
+		#else, return false	
 
 
 # 3. Initial Solution
@@ -15,6 +26,7 @@
 # Don't forget to check on intialization for a card length
 # of exactly 16 digits
 class CreditCard
+	def initia
 
 end
 
