f3f5df01ecb53b418c654e0437d763004a533513
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-04 22:26:27 -0400

    updated gps

--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -8,7 +8,48 @@
 
 
 # Our Refactored Solution
+def bakery_num(num_of_people, fav_food)
+  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1} # creates a hash of serving sizes
+  #set quantities of items to zero
+  pie_qty = 0 
+  cake_qty = 0
+  cookie_qty = 0
+  #has_fave set to default as false
+  fave_match = false
+#iterate through each key of hash, and when found, change has_fav to true
+  my_list.each_key do |k|
+      if k == fav_food
+          has_fave = true
+      end
+  end
+#if fav_food isn't in hash, raise error
+    if has_fave == false
+        raise ArgumentError.new("You can't make that food")
+    else
+        fav_food_qty = my_list.values_at(fav_food)[0] #if fav_found in hash, give value of key
+        if num_of_people % fav_food_qty == 0 # if remainder of num_of_people / fav_food_qty is 0, it returns equal number of food 
+            num_of_food = num_of_people / fav_food_qty
+            return "You need to make #{num_of_food} #{fav_food}(s)."
+    
+        else num_of_people % fav_food_qty != 0 #if remainder of number of people is NOT equal to zero
 
+                                      
+            while num_of_people > 0 
+                if num_of_people / my_list["pie"] > 0 #if number of people divided by value at key pie in hash is greater than zero
+                    pie_qty = num_of_people / my_list["pie"] #set pie quantity to number of people divided by value at key pie hash
+                    num_of_people = num_of_people % my_list["pie"] #re-set number of people to remainder of people divuded by value at key pie in hash
+                elsif num_of_people / my_list["cake"] > 0 #do the same thing for cake
+                    cake_qty = num_of_people / my_list["cake"]
+                    num_of_people = num_of_people % my_list["cake"]
+                else
+                    cookie_qty = num_of_people #losers gets a cookie
+                    num_of_people = 0
+                end
+            end
+            return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)." #print quantities needed for fav_foods
+        end
+    end
+end
 
 
 
